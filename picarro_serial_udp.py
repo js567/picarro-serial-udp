ecad8ff467b4cdda64f1246df8991d8c6f733409
Jack Stevenson <75865953+js567@users.noreply.github.com>
2021-08-27 18:45:03 +0000

    Update picarro_serial_udp.py

--- picarro_serial_udp.py
+++ picarro_serial_udp.py
@@ -1,8 +1,22 @@
 import serial
-import serial.tools.list_ports
+#import serial.tools.list_ports
 import socket
 
-ser = serial.Serial()
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-print(list(serial.tools.list_ports.comports()))
-print('hello')
+ser = serial.Serial("COM2", 19200, timeout=5)
+
+while True:
+
+    try:
+
+        s = ser.readline()
+        print(s)
+        print("Sending UDP")
+        sock.sendto(bytes(str(s), "utf-8"), ("255.255.255.255", 30330))
+        print("Done")
+
+    except Exception:
+        break
