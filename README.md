3b3479f745d112d6e6ac017bea75090a494820fe
Jack Stevenson <75865953+js567@users.noreply.github.com>
2021-09-09 14:36:16 -0700

    dev commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # picarro-serial-udp
 Simple Python script for converting serial from Picarro gas composition sensor to UDP for network data transfer
+
+## To set up for RCRV:
+
+1. Attach a null modem 9-pin serial cable to the back of the Picarro analyzer between the COM1 and COM2 ports
+2. Start the picarro_serial_udp.py to confirm that the program is properly collecting data
+
+FOR AUTOMATIC STARTUP
+1. Run the included .exe file and start it to ensure it has the same output as the .py file
+   (the .py file will need to be stopped so that the serial port is free)
+2. In file explorer, navigate to C:\Users\username\Appdata\Roaming\Microsoft\Start Menu\Programs\Startup
+3. Place the .exe file in the Startup folder
+4. Restart the machine to ensure that data is collected and transferred properly 
+
+TO EDIT AUTOMATIC STARTUP FILE
+1. Remove file from Startup folder
+2. pip install pyinstaller
+3. Edit .py file 
+4. pyinstaller picarro_serial_udp.py
+5. .exe file can be found in 'dist' folder in same directory
+
+Currently set up to 
--- picarro_serial_udp.py
+++ picarro_serial_udp.py
@@ -1,11 +1,23 @@
-import serial
-#import serial.tools.list_ports
-import socket
+"""
+Picarro Serial-UDP Transfer Script
 
+This script is used to transfer the serial data from the Picarro gas analyzer over UDP to OpenRVDAS.
+
+"""
+
+import serial
+import socket
+import logging
+import time
+import datetime
+
+# Setup socket for UDP transfer
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
+
+# Setup serial port on Picarro
 ser = serial.Serial("COM2", 19200, timeout=5)
 
 while True:
@@ -13,10 +25,20 @@ while True:
     try:
 
         s = ser.readline()
-        print(s)
-        print("Sending UDP")
-        sock.sendto(bytes(str(s), "utf-8"), ("255.255.255.255", 30330))
-        print("Done")
+        if len(s) <= 1:
+            time.sleep(10)
+        elif len(s) > 1:
+            timestamp = datetime.datetime.now()
+            s = str(timestamp) + ' ' + str(s)
+            print(s)
+            print("Sending UDP")
+            sock.sendto(bytes(str(s), "utf-8"), ("255.255.255.255", 30330))
+            print("Done")
 
-    except Exception:
+    except KeyboardInterrupt:
+        logging.warning("Program stopped")
+        break
+
+    except Exception as e:
+        logging.exception("Exception occurred")
         break
